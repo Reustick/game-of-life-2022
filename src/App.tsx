1ad52a2593820d6cf008e3eb24d0bedace3fe082
Rustam <rustam391@gmail.com>
2022-01-08 23:59:24 +0300

    настроит предварительную конфгурацию

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Game of life</h1>
+      <h2>Hello World</h2>
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackRules = require("./webpackRules");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    alias: {
+      types: path.resolve(__dirname, "src/types"),
+      components: path.resolve(__dirname, "src/components"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "./index.js",
+    publicPath: "/",
+    // https://github.com/GoogleChromeLabs/worker-plugin/issues/20
+    globalObject: "(typeof self!='undefined'?self:global)",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.worker\.(ts|js)$/,
+        use: { loader: "worker-loader" },
+      },
+      ...webpackRules,
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
